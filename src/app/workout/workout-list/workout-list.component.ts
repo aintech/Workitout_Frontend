7c2a1643b61a35892e29edd540657d013e5048d6
Aintech <aintech@mail.ru>
2018-10-20 21:17:31 +0300

    Workout deletion

--- src/app/workout/workout-list/workout-list.component.ts
+++ src/app/workout/workout-list/workout-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Workout } from '../workout.model';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-workout-list',
@@ -11,13 +12,25 @@ export class WorkoutListComponent implements OnInit {
 
   workouts: Workout[];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private router: Router) {}
 
   ngOnInit() {
     this.http.get('/back/workouts').subscribe(
       data => {
         this.workouts = <Workout[]>data;
       }
+    );
+  }
+
+  deleteWorkout (workout: Workout) {
+    const idx: number = this.workouts.indexOf(workout, 0);
+    this.workouts.splice(idx, 1);
+    this.http.delete('/back/workouts/' + workout.id).subscribe(
+      res => {
+        this.router.navigate(['/workout-list']);
+      },
+      (err) => { console.log(err); }
     );
   }
 }
