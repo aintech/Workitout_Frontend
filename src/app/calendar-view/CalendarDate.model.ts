b08b52fd2d2401ff3e9539149760afd37f21b616
aintech <aintech@mail.ru>
2018-10-23 18:56:45 +0300

    Adjust Calendar view

--- src/app/calendar-view/CalendarDate.model.ts
+++ src/app/calendar-view/CalendarDate.model.ts
@@ -1,8 +1,11 @@
 export class CalendarDate {
-  
+
   date: Date;
+
+  dayVal: string;
 
   constructor(date: Date) {
     this.date = date;
+    this.dayVal = date.getDate().toString();
   }
 }
--- src/app/calendar-view/calendar-view.component.ts
+++ src/app/calendar-view/calendar-view.component.ts
@@ -8,7 +8,7 @@ import { CalendarDate } from './CalendarDate.model';
 })
 export class CalendarViewComponent implements OnInit {
 
-  dates: CalendarDate[] = [];
+  dates: CalendarDate[][];
 
   months: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
@@ -21,20 +21,23 @@ export class CalendarViewComponent implements OnInit {
     this.monthYear = this.months[currDate.getMonth()] + " of " + currDate.getFullYear();
     const firstDay: Date = new Date(currDate.getFullYear(), currDate.getMonth(), 1);
     const lastDay: Date = new Date(currDate.getFullYear(), currDate.getMonth() + 1, 0)
-    let staring: number =  7 + firstDay.getDay();
-    let ending: number = 7 + lastDay.getDate();
+    let starting: number =  7 + firstDay.getDay() - 1;
+    let ending: number = 7 + lastDay.getDate() - 1;
 
-    for (let i: number = 0; i < 7 * 6; i++) {
-      if (i < staring) {
-        this.dates.push(new CalendarDate(new Date()));
-      }
-      if (i >= staring && i <= ending) {
-        this.dates.push(
-      }
-      if (i > ending) {
-        this.dates.push(new CalendarDate(new Date()));
+    let beginingDate: Date = new Date(firstDay.getFullYear(), firstDay.getMonth(), firstDay.getDate() - starting);
+
+    this.dates = [];
+    let index:number = 0;
+    for (let i:number = 0; i < 6; i++) {
+      this.dates[i] = [];
+      for (let j: number = 0; j < 7; j++) {
+        this.dates[i].push(new CalendarDate(new Date(beginingDate.getFullYear(), beginingDate.getMonth(), beginingDate.getDate() + index)));
+        index++;
       }
     }
   }
 
+  onCalendarClick () {
+    console.log("click");
+  }
 }
